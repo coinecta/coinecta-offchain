d84a50bf6ba41ebd9bb52ddea3b8bd98ded7ccea
rjlacanlaled <rjlacanlaled@gmail.com>
2024-03-01 00:02:25 +0800

    Add error handling

--- src/Coinecta.API/Extensions/TransactionBuilderExtension.cs
+++ src/Coinecta.API/Extensions/TransactionBuilderExtension.cs
@@ -3,6 +3,7 @@ using CardanoSharp.Wallet.Enums;
 using CardanoSharp.Wallet.Extensions.Models;
 using CardanoSharp.Wallet.Extensions.Models.Transactions;
 using CardanoSharp.Wallet.Models.Transactions;
+using CardanoSharp.Wallet.Models.Transactions.TransactionWitness.PlutusScripts;
 using CardanoSharp.Wallet.TransactionBuilding;
 using CardanoSharp.Wallet.Utilities;
 
@@ -11,15 +12,22 @@ namespace Coinecta.API.Extensions;
 public static class TransactionBuilderExtension
 {
 
-    public static Transaction BuildAndSetExUnits(this ITransactionBuilder builder, NetworkType networkType)
+    public static Transaction BuildAndSetExUnits(this ITransactionBuilder builder, NetworkType networkType, List<IPlutusData>? datums = null)
     {
         Transaction tx = builder.Build();
         CsBindgen.TransactionEvaluation txEvalResults = CsBindgen.UPLCMethods.GetExUnits(tx, networkType);
 
+        if (txEvalResults.Error != null)
+        {
+            throw new Exception($"Error evaluating transaction: {txEvalResults.Error}");
+        }
+
+        datums ??= [];
+
         // add ex units to transaction
         tx.TransactionWitnessSet.Redeemers = txEvalResults.Redeemers!;
         tx.TransactionBody.ScriptDataHash = ScriptUtility
-            .GenerateScriptDataHash(txEvalResults.Redeemers!, [], CostModelUtility.PlutusV2CostModel.Serialize());
+            .GenerateScriptDataHash(txEvalResults.Redeemers!, datums, CostModelUtility.PlutusV2CostModel.Serialize());
 
         return tx;
     }
