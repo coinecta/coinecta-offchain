0d54cdfc951fda7c5d347f349642181aae6480c2
rjlacanlaled <rjlacanlaled@gmail.com>
2024-02-29 17:45:57 +0800

    Refactor BuildAndSetExUnits method in TransactionBuilderExtension.cs

--- src/Coinecta.API/Extensions/TransactionBuilderExtension.cs
+++ src/Coinecta.API/Extensions/TransactionBuilderExtension.cs
@@ -13,13 +13,13 @@ public static class TransactionBuilderExtension
 
     public static Transaction BuildAndSetExUnits(this ITransactionBuilder builder, NetworkType networkType)
     {
-        var tx = builder.Build();
-        var txEvalResults = CsBindgen.UPLCMethods.GetExUnits(tx, networkType);
+        Transaction tx = builder.Build();
+        CsBindgen.TransactionEvaluation txEvalResults = CsBindgen.UPLCMethods.GetExUnits(tx, networkType);
 
         // add ex units to transaction
         tx.TransactionWitnessSet.Redeemers = txEvalResults.Redeemers!;
         tx.TransactionBody.ScriptDataHash = ScriptUtility
-            .GenerateScriptDataHash(txEvalResults.Redeemers!, null, CostModelUtility.PlutusV2CostModel.Serialize());
+            .GenerateScriptDataHash(txEvalResults.Redeemers!, [], CostModelUtility.PlutusV2CostModel.Serialize());
 
         return tx;
     }
