0b9df25ceaa0bcf5d155a00eff1aa4d85b01b888
rjlacanlaled <rjlacanlaled@gmail.com>
2024-03-01 15:53:20 +0800

    Change output reference to list

--- src/Coinecta.API/Models/Request/ClaimStakeRequest.cs
+++ src/Coinecta.API/Models/Request/ClaimStakeRequest.cs
@@ -2,6 +2,6 @@ namespace Coinecta.API.Models.Request;
 
 public record ClaimStakeRequest
 {
-    public OutputReference StakeUtxoOutputReference { get; init; } = default!;
+    public IEnumerable<OutputReference> StakeUtxoOutputReferences { get; init; } = default!;
     public IEnumerable<string> WalletUtxoListCbor { get; init; } = default!;
 }
