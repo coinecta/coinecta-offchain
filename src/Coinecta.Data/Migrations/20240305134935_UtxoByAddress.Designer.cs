f075b3a1a807f7c3e0af47dd892cf4b8d88f0474
rjlacanlaled <rjlacanlaled@gmail.com>
2024-03-06 02:07:16 +0800

    Add UtxoByAddress migration files

--- src/Coinecta.Data/Migrations/20240305134935_UtxoByAddress.Designer.cs
+++ src/Coinecta.Data/Migrations/20240305134935_UtxoByAddress.Designer.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using Coinecta.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Coinecta.Data.Migrations
 {
     [DbContext(typeof(CoinectaDbContext))]
-    partial class CoinectaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240305134935_UtxoByAddress")]
+    partial class UtxoByAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -170,6 +173,7 @@ namespace Coinecta.Data.Migrations
                         .HasColumnType("integer");
 
                     b.Property<byte[]>("TxOutCbor")
+                        .IsRequired()
                         .HasColumnType("bytea");
 
                     b.HasKey("Address", "Slot", "TxHash", "TxIndex", "Status");
