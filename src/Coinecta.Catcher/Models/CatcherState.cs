352929a0e1b5a8c3e456d3eee97cb5f83d8dccac
rjlacanlaled <rjlacanlaled@gmail.com>
2024-03-06 02:08:57 +0800

    Add Address properties

--- src/Coinecta.Catcher/Models/CatcherState.cs
+++ src/Coinecta.Catcher/Models/CatcherState.cs
@@ -1,5 +1,5 @@
-using System.Security.Cryptography.X509Certificates;
 using CardanoSharp.Wallet.Models;
+using CardanoSharp.Wallet.Models.Addresses;
 using CardanoSharp.Wallet.Models.Keys;
 using Coinecta.Data.Models.Reducers;
 using PublicKey = CardanoSharp.Wallet.Models.Keys.PublicKey;
@@ -11,7 +11,11 @@ public class CatcherState
     public List<StakePoolByAddress>? CurrentStakePoolStates { get; set; }
     public Utxo? CurrentCertificateUtxoState { get; set; }
     public Utxo? CurrentCollateralUtxoState { get; set; }
-    public IEnumerable<PendingStakeRequest> PendingExecutionStakeRequests { get; set; } = [];
+    public List<PendingStakeRequest> PendingExecutionStakeRequests { get; set; } = [];
     public PublicKey CatcherPublicKey { get; set; } = default!;
     public PrivateKey CatcherPrivateKey { get; set; } = default!;
+    public Address CatcherAddress { get; set; } = default!;
+    public string SubmitApiUrl { get; set; } = default!;
+    public string CatcherCertificatePolicyId { get; set; } = default!;
+    public string CatcherCertificateAssetName { get; set; } = default!;
 }
