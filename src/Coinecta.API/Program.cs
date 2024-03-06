602eb7a3cb9ce987d0bbfc1d88cf4fec86d6d7fa
Clark Alesna <clark_alesna@hotmail.com>
2024-03-06 20:23:28 +0800

    Stake Position Initial and Bonus values are now ulong

--- src/Coinecta.API/Program.cs
+++ src/Coinecta.API/Program.cs
@@ -205,8 +205,8 @@ app.MapPost("/stake/positions", async (IDbContextFactory<CoinectaDbContext> dbCo
         Dictionary<string, ulong> asset = sp.Amount.MultiAsset[policyId!];
         string assetNameAscii = Encoding.ASCII.GetString(Convert.FromHexString(asset.Keys.FirstOrDefault()!));
         ulong total = asset.Values.FirstOrDefault();
-        double initial = total / (1 + interest);
-        double bonus = total - initial;
+        ulong initial = (ulong)(total / (1 + interest));
+        ulong bonus = total - initial;
         DateTimeOffset unlockDate = DateTimeOffset.FromUnixTimeMilliseconds((long)sp.LockTime);
 
         return new
@@ -216,9 +216,11 @@ app.MapPost("/stake/positions", async (IDbContextFactory<CoinectaDbContext> dbCo
             UnlockDate = unlockDate,
             Initial = initial,
             Bonus = bonus,
-            Interest = interest
+            Interest = interest,
+            sp.TxHash,
+            sp.TxIndex
         };
-    });
+    }).OrderByDescending(sp => sp.UnlockDate).ToList();
 
     return Results.Ok(result);
 })
