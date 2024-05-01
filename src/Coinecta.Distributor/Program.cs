66125daf7c3b24a6bb6c1ae5651716908699b280
rjlacanlaled <rjlacanlaled@gmail.com>
2024-05-01 13:40:36 +0800

    Add HTTP retry policy for submit API

--- src/Coinecta.Distributor/Program.cs
+++ src/Coinecta.Distributor/Program.cs
@@ -1,13 +1,27 @@
 using Coinecta.Distributor;
+using Polly;
+using Polly.Extensions.Http;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
-
 builder.Services.AddHttpClient("SubmitApi", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["CardanoSubmitApiUrl"]!);
-});
+})
+.AddPolicyHandler(GetRetryPolicy());
 
 IHost host = builder.Build();
 host.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+        .WaitAndRetryAsync(8, retryAttempt => TimeSpan.FromMilliseconds(Math.Pow(3, retryAttempt)),
+            onRetry: (outcome, timespan, retryAttempt, context) =>
+            {
+                Console.WriteLine($"Retry {retryAttempt} after {timespan.TotalSeconds} seconds");
+            });
+}
