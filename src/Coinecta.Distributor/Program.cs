c6fdd3b49c963e025b14db755eb25a0037f952ab
Rj Lacanlale <rjlacanlaled@gmail.com>
2024-05-01 18:48:06 +0800

    fix(distributor): handle errors when submitting airdrop tx (#14)
    * Add Polly as dependency for HTTP retry policy
    
    * Add HTTP retry policy for submit API
    
    * Refactor empty string checks to use string.IsNullOrEmpty function
    
    * Handle all non-success status code
    
    * Move policy directly to the builder instead of having a separate static function
    
    * Rename files to avoid confusion.
    
    distribution.csv -> initial file
    processing.csv -> when the program starts processing the entries
    processed-date.csv -> for successfully processed transactions
    unprocessed-date.csv -> for unsuccessful transactions that needs to be manually checked
    
    * Remove unnecessary comment

--- src/Coinecta.Distributor/Program.cs
+++ src/Coinecta.Distributor/Program.cs
@@ -1,13 +1,25 @@
 using Coinecta.Distributor;
+using Polly;
+using Polly.Extensions.Http;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
-
 builder.Services.AddHttpClient("SubmitApi", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["CardanoSubmitApiUrl"]!);
-});
+})
+.AddPolicyHandler(HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .OrResult(msg => !msg.IsSuccessStatusCode)
+    .WaitAndRetryAsync(8, retryAttempt => TimeSpan.FromMilliseconds(Math.Pow(3, retryAttempt)),
+        onRetry: (outcome, timespan, retryAttempt, context) =>
+        {
+            Console.WriteLine($"Retry {retryAttempt} after {timespan.TotalSeconds} seconds due to {outcome.Result?.StatusCode}");
+        }
+    )
+);
 
 IHost host = builder.Build();
 host.Run();
+
